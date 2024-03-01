49d7d777f09749cc306185b04c761ad1e064d86f
NihanShahriarPalock <nihanshahriar000@gmail.com>
2024-03-01 16:29:07 +0600

    Bring Data from API in console

--- phone.js
+++ phone.js
@@ -1,1 +1,16 @@
-{\rtf1}
+
+const loadPhone = async () => {
+    const res = await fetch("https://openapi.programming-hero.com/api/phones?search=iphone");
+    const data = await res.json();
+    const phones = data.data;
+    // console.log(phones);
+    displayPhone(phones);
+}
+
+loadPhone();
+
+const displayPhone = (phones) => {
+    phones.forEach(phone => {
+        console.log(phone);
+    });
+}
